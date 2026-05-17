45f7315caf75733c546ab39b396e52c7d31129ae
KirkyCoding <lualhatikirk13@gmail.com>
2026-05-18 03:28:59 +0800

    Force email SMTP to use IPv4

--- server.js
+++ server.js
@@ -233,6 +233,7 @@ const mailer = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT || 465),
   secure: String(process.env.EMAIL_SECURE || "true") === "true",
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
